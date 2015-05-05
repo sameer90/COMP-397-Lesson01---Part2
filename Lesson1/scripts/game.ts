0522b6b7133bbf4ef5a2c3b6c4e6e4b57cd52b15
sameer90 <sameersharma>
2015-05-05 11:25:54 -0400

    Initial 3

---
+++ Lesson1/scripts/game.ts
@@ -0,0 +1,30 @@
+﻿//class Person {
+//    constructor() {
+//    }
+//    //public methods
+//    public saysHello() {
+//        console.log("Hello World");
+//    }
+//}
+
+var stage: createjs.Stage;
+var canvas = document.getElementById("canvas");
+function init() {
+//    var myPerson = new Person();
+  //  myPerson.saysHello();
+    stage = new createjs.Stage(canvas);
+    createjs.Ticker.setFPS(60);
+    createjs.Ticker.on("tick",gameLoop);
+
+    
+} 
+function gameLoop() {
+    stage.update();
+}
+function main() {
+    console.log("Game is running properly.........");
+    var helloText: createjs.Text = new createjs.Text("Hello World", "40 px Consolas", "#000000");
+    helloText.x = 30;
+    helloText.y = 200;
+    stage.addChild(helloText);
+}
